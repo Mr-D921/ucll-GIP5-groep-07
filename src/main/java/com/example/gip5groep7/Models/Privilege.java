57ad2c90145bd8c47cf0cd450c314df6eed4efda
Dries Van Goidsenhoven <dries.vangoidsenhoven@student.ucll.be>
2023-02-16 15:50:26 +0100

    privilege toegevoegd

---
+++ src/main/java/com/example/gip5groep7/Models/Privilege.java
@@ -0,0 +1,35 @@
+package com.example.gip5groep7.Models;
+
+import jakarta.persistence.*;
+
+import java.util.Collection;
+
+@Entity
+public class Privilege {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column
+    private String name;
+
+//    @ManyToMany(mappedBy = "privileges")
+//    private Collection<Role> roles;
+//
+//    public Collection<Role> getRoles() {
+//        return roles;
+//    }
+//
+//    public void setRoles(Collection<Role> roles) {
+//        this.roles = roles;
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/example/gip5groep7/Services/PrivilegeService.java
@@ -0,0 +1,8 @@
+package com.example.gip5groep7.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PrivilegeService {
+
+}
