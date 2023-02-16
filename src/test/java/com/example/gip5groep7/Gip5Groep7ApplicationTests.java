b3307990607e015896e00a52267f6647ea8702f4
Dries Van Goidsenhoven <dries.vangoidsenhoven@student.ucll.be>
2023-02-16 12:44:16 +0100

    Basis Spring boot project aangemaakt.

---
+++ src/test/java/com/example/gip5groep7/Gip5Groep7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gip5groep7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gip5Groep7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
