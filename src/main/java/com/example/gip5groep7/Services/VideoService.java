16029c5091650c55ceb7d183a242b7716da42f4a
WoutDeBoeck <wout.deboeck@student.ucll.be>
2023-02-16 17:13:18 +0100

    Added Model, Repo & Controller classes for video

---
+++ src/main/java/com/example/gip5groep7/Services/VideoService.java
@@ -0,0 +1,15 @@
+package com.example.gip5groep7.Services;
+
+import com.example.gip5groep7.Repositories.VideoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VideoService {
+
+    @Autowired
+    private VideoRepository videoRepo;
+
+    //TODO: implement video service
+
+}
