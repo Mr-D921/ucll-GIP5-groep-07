ad6cbed7e8f35a1460eb174abe300c393f685733
Mieke <mieke.laporte@student.ucll.be>
2023-02-19 13:02:29 +0100

    user model (not done yet)

---
+++ src/main/java/com/example/gip5groep7/Models/User.java
@@ -0,0 +1,92 @@
+package com.example.gip5groep7.Models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "USER") //niet zeker of dit klopt
+public class User {
+    @Id
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "phoneNumber")
+    private String phoneNumber;
+
+    @Column(name = "dateOfBirth")
+    private Date dateOfBirth;
+    //todo add Roles: Collection<Role>
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public User() {
+    }
+
+    public User(int id, String username, String password, String name, String phoneNumber, Date dateOfBirth) {
+        setId(id);
+        setUsername(username);
+        setPassword(password);
+        setName(name);
+        setPhoneNumber(phoneNumber);
+        setDateOfBirth(dateOfBirth);
+    }
+}
