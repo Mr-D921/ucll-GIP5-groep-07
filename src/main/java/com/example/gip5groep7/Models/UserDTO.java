a080ca23230b4189ac963fbbe627e9882d1017e2
Mieke.L <mieke.laporte@student.ucll.be>
2023-02-20 12:38:35 +0100

    getters and setters in UserDTO

--- src/main/java/com/example/gip5groep7/Models/UserDTO.java
+++ src/main/java/com/example/gip5groep7/Models/UserDTO.java
@@ -10,4 +10,53 @@ public class UserDTO {
     public String phoneNumber;
     public Date dateOfBirth;
     //todo add Roles
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 }
