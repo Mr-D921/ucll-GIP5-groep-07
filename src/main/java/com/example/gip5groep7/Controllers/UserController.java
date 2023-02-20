56171115fe19a77799871b0f835128f091508635
Mieke.L <mieke.laporte@student.ucll.be>
2023-02-20 12:39:05 +0100

    creation of UserController and UserService

---
+++ src/main/java/com/example/gip5groep7/Controllers/UserController.java
@@ -0,0 +1,7 @@
+package com.example.gip5groep7.Controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+}
---
+++ src/main/java/com/example/gip5groep7/Services/UserService.java
@@ -0,0 +1,10 @@
+package com.example.gip5groep7.Services;
+
+import com.example.gip5groep7.Repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+
+}
