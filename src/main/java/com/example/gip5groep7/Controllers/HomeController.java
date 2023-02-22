7649b9b5b179281df632cca0137b2f9816b15946
Omer Faruk Guven <70398430+ofguven@users.noreply.github.com>
2023-02-22 12:28:41 +0100

    video page index

--- src/main/java/com/example/gip5groep7/Controllers/HomeController.java
+++ src/main/java/com/example/gip5groep7/Controllers/HomeController.java
@@ -3,12 +3,19 @@ package com.example.gip5groep7.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String helloworld(Model model){
+    public String home(Model model){
 
         return "home/index";
+    }
+    @RequestMapping("/{videoCode}")
+    public String redirectToVideo(@PathVariable String videoCode, Model model){
+        model.addAttribute(videoCode);
+        return "video/index";
     }
 }
